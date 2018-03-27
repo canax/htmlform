4cd31e6995e30fcb451bd8120edb2d11554c7a42
MagnusGreiff <mangegreiff@gmail.com>
2018-03-27 10:54:15 +0200

    Added ability to set maxlength on input.

--- src/HTMLForm/FormElementInput.php
+++ src/HTMLForm/FormElementInput.php
@@ -22,7 +22,7 @@ class FormElementInput extends FormElement
         return <<<EOD
 <{$wrapperElement}{$wrapperClass}>
 <label for='$id'>$label</label>$brAfterLabel
-<input id='$id'{$type}{$class}{$name}{$value}{$autofocus}{$required}{$readonly}{$placeholder}{$title}{$multiple}{$pattern}{$max}{$min}{$step}/>
+<input id='$id'{$type}{$class}{$name}{$value}{$autofocus}{$required}{$readonly}{$placeholder}{$title}{$multiple}{$pattern}{$maxlength}{$max}{$min}{$step}/>
 {$messages}
 </{$wrapperElement}>
 <p class='cf-desc'>{$description}</p>
